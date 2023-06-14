93d94a70254ffbd7fd277b581255d66c0effc8a8
ruffi0 <matthew@motorcity.host>
2023-06-14 14:22:31 -0400

    revert back to og

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,16 @@
 import { useAuth } from '~/store/auth'
-import { storeToRefs } from 'pinia'
-const msg = 'You must be logged in to view this page.'
+const msg =
+  'You must be logged in to view this page. Redirecting back to home page.'
 
-export default defineNuxtRouteMiddleware(({ redirect, from }) => {
-  const auth = useAuth()
-  
-  const { isLoggedIn, user } = storeToRefs(auth)
+export default defineNuxtRouteMiddleware((to, from) => {
+  const user = useAuth()
 
-  // Check if the middleware is called on the client-side
-  if (process.client) {
-    // Check if the previous route is not the current route
-    // This check is to avoid alert and redirection when accessing the page directly via the URL
-    if (from?.path !== null && from?.path !== '/') {
-      if (user.isLoggedIn) {
-        
-        // Redirect to the login page or any other appropriate page
-        
-      } else{
-        window.alert(msg)
-        return navigateTo('/')
-      }
+  if (!user.isLoggedIn) {
+    if (process.client) {
+      window.alert(msg)
     }
+    console.log(msg)
+
+    return navigateTo('/')
   }
 })
